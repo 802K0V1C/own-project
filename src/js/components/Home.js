00cc7ea4d70c060e0fa4ca18b16a528341cb6b52
Artur Bożek <arturhubert.bozek@gmail.com>
2023-07-05 10:59:56 +0200

    add js and product scss

---
+++ src/js/components/Home.js
@@ -0,0 +1,24 @@
+import { select, templates } from '../settings.js';
+import utils from '../utils.js';
+class Home {
+  constructor() {
+    const thisHome = this;
+
+    thisHome.render();
+  }
+
+  render(){
+    const thisHome = this;
+        
+    const generatedHTML = templates.homeWidget();
+    const generatedDom = utils.createDOMFromHTML(generatedHTML);
+    const homeWrapper = document.querySelector(select.containerOf.home);
+
+    homeWrapper.appendChild(generatedDom);
+
+    thisHome.dom = {};
+
+  }
+}
+
+export default Home;
